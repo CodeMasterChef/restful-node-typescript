6f91db9ff829deea4120a11a46fede7a8c7950ee
Ninh Le <levanninh0101@gmail.com>
2018-11-03 12:51:34 +0700

    remove code

--- src/server.ts
+++ src/server.ts
@@ -9,13 +9,5 @@ const server = app.listen( app.get("port"), app.get("address"), () => {
             )
   });
 
-  
-// const server = app.listen(app.get("port"), () => {
-//     console.log(
-//         "App is running on http://localhost:%d in %s mode",
-//         app.get("port"),
-//         app.get("env")
-//     )
-// });
 
 export default server;
